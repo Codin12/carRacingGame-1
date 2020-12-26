c42795e28579fc7498affbf35c65eb9bcf62ae1a
Codin12 <73191419+Codin12@users.noreply.github.com>
2020-12-26 17:58:17 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var gameState=0;
+var database;
+var playerCount=0;
+var form,player,game;
+
+function setup(){
+    createCanvas(400,400);
+   
+    database=firebase.database();
+    console.log(database);
+
+    game=new Game();
+    game.getState();
+    game.start();
+    
+}
+
+function draw(){
+    background("white");
+    
+    drawSprites();
+}
+
+
+
